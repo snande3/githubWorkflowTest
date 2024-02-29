8c267551cc05405279ce82deafe8947f2f59d0b1
Samihan Nandedkar <svn2998@gmail.com>
2024-02-29 17:07:39 -0600

    Adding initial data

---
+++ test-script.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+//  Get application settings and secrets from config.json file
+const configPath = path.join(__dirname, "./config.json");
+const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+
+
+console.log('COnfigs:', config);
